52d63024698dd16706b54dec58fcb28b33edb429
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-14 13:45:57 +0900

    work02：定数
    constを用いて定数を宣言した後、「=」を用いて値の再代入を行うプログラムを記述してください。
    下記のようなエラーが出現することを確認してください。

---
+++ htdocs/work02/work02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>WORK023</title>
+</head>
+<body>
+<?php
+	const HEIGHT = 200;	// 定数HEIGHTを定義
+
+	HEIGHT = 300;		// 値を変えるとエラー
+
+	//print "aaaaaa";
+?>
+</body>
+</html>
