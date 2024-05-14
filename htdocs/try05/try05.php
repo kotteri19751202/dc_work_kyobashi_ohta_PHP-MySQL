1f51d72da028fc3d9904b166e49fe0b8a7b09280
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-14 14:23:51 +0900

    try05：代入演算子
    下記の例題と同様に、算術演算子を使用したプログラムを作成し、実行してみましょう。

---
+++ htdocs/try05/try05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY05</title>
+</head>
+<body>
+<?php
+	$iNum = 10;
+	print '<p>$iNum : '.$iNum.'</p>';
+	// 値を加算して変数に代入
+	$iNum += 2;		// 1
+	print '<p>$iNum += 2 : '.$iNum.'</p>';
+?>
+</body>
+</html>
