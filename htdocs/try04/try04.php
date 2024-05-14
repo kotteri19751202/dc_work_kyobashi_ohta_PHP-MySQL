cc0e49e3fcee9462ec819e8d28bbb74d56b571c1
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-14 14:15:59 +0900

    try04：算術演算子
    下記の例題と同様に、算術演算子を使用したプログラムを作成し、実行してみましょう。

---
+++ htdocs/try04/try04.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY04</title>
+</head>
+<body>
+<?php
+	$iNum = 0;
+	// 加算
+	$iNum = 1 + 2;	// 3
+	print "<p>1 + 2 = ".$iNum."</p>";
+	// 減算
+	$iNum = 3 -2;		// 1
+	print "<p>3 - 2 = ".$iNum."</p>";
+	// 乗算
+	$iNum = 3 * 4;	// 12
+	print "<p>3 * 4 = ".$iNum."</p>";
+	// 除算
+	$iNum = 6 / 2;	// 3
+	print "<p>6 / 2 = ".$iNum."</p>";
+	// 剰余
+	$iNum = 5 % 4;	// 1
+	print "<p>5 % 4 = ".$iNum."</p>";
+?>
+</body>
+</html>
