3cde09ab4f64fa771a036c93699fa609bccf85d1
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-14 14:05:03 +0900

    work03：4. データ型
    上記全ての変数を宣言（値が格納された状態）して、画面上に出力して確認してください。

---
+++ htdocs/work03/work03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>WORK03</title>
+</head>
+<body>
+<?php
+	$iTest = 1;
+	$strTest = "test";
+	$bTest = false;
+	$nullTest = null;
+	$emptyTest = "";
+
+	print $iTest."<br>";
+	print $strTest."<br>";
+	print $bTest."<br>";
+	print $nullTest."<br>";
+	print $emptyTest."<br>";
+?>
+</body>
+</html>
