edeffcee89c66ab15eea8aeec855d99dc34328be
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-16 13:13:22 +0900

    try06：インクリメント演算子とデクリメント演算子
    下記の例題と同様に、インクリメント演算子とデクリメント演算子を使用したプログラムを作成し、実行してみましょう。

---
+++ htdocs/try06/try06.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY06</title>
+</head>
+<body>
+<?php
+	$i = 1;
+	$i++;	// 2
+	echo '<p>i: '.$i.'</p>';
+	// 値を加算して変数に代入
+
+	$j = 2;
+	$j--;	// 1
+	echo '<p>j : '.$j.'</p>';
+?>
+</body>
+</html>
