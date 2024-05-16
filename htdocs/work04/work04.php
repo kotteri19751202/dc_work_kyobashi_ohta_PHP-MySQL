491cc7ad6e013a4436e288c94d7d24c111650455
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-16 13:57:50 +0900

    work04：文字列演算子
    下記の要件を満たすプログラムを実装してください。
    「109439032に39443083を加え、4934で割った答え」を、文字列と連結させ、
    
    計算の答えは[上記で求めた答えの数値]です。
    
    と表示させてください。

---
+++ htdocs/work04/work04.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>WORK04</title>
+</head>
+<body>
+<?php
+	$iNum = 109439032;
+	$iNum += 39443083;
+	$iAnswer = $iNum / 4934;
+
+	print "計算の答えは".$iAnswer."です。";
+?>
+</body>
+</html>
