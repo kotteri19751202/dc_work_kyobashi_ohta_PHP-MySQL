66174b9140880e1cdc562e8dc3d2fc813ca827a2
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-16 13:38:36 +0900

    try：文字列演算子
    下記の例題と同様に、文字列演算子を使用したプログラムを作成し、実行してみましょう。

---
+++ htdocs/try09/try09.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY09</title>
+</head>
+<body>
+<?php
+	$strString = "Hello"."World";
+	print $strString;
+
+?>
+</body>
+</html>
