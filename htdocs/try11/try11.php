32b06cdc3020a2f0fac2008cb0d9dd0a187f44b9
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-21 10:55:51 +0900

    try11：if文をコロン構文で書く方法
    下記のプログラムを作成し、実行してみましょう

---
+++ htdocs/try11/try11.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY11</title>
+</head>
+<body>
+<?php
+	$iScore = rand( 0, 100 );	// 0～100までのランダムな数値を取得
+
+	print "<p>iScore: ".$iScore."</p>";
+	print "<p>$iScore == 100 : ";
+	var_dump($iScore == 100 );
+	print " </p>";
+
+	print "<p>$iScore >= 60 : ";
+	var_dump($iScore >= 60 );
+	print " </p>";
+
+	if( $iScore == 100 )
+		print "<p>満点です。</p>";
+	else
+	if( $iScore >= 60 ):
+		print "<p>合格です。</p>";
+	else:
+		print "<p>不合格です。</p>";
+	endif;
+?>
+</body>
+</html>
