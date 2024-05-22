3b276990f5dd58fd8a6ec97e362ff904d8358642
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-05-22 13:09:44 +0900

    try17：while 文
    下記のプログラムを作成し、実行してみましょう

---
+++ htdocs/try17/try17.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY17</title>
+</head>
+<body>
+	<?php
+		$i = 2;
+		while( $i <= 20 )
+		{
+			echo $i;
+			$i++;
+		}
+	?>
+</body>
+</html>
