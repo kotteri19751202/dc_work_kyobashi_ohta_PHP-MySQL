53a42da17d7e9cf15c13dcf698c39614962d2b3b
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-08-07 13:49:58 +0900

    work34：疑問符プレースホルダを利用した実装方法
    プリペアドステートメント（疑問符プレースホルダ）を利用して、SELECT文を作成して実行させるプログラムを実装してください。

---
+++ htdocs/work34/work34.php
@@ -0,0 +1,81 @@
+<?php 
+	define( "DSN", "mysql:host=localhost;dbname=xb513874_wa8fy" ); 
+	define( "DB_USER_NAME", "xb513874_t7dq0" ); 
+	define( "DB_PASS_WORD", "7op8tds1dz" );   
+
+ ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>WORK34</title>
+</head>
+<body>
+	<?php
+		try
+		{
+			// データベースへ接続
+			$db = new PDO( DSN, DB_USER_NAME, DB_PASS_WORD );
+			//PDOのエラー時にPEOExceptionが発生するように設定
+			$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+			// トランザクション開始
+			$db->beginTransaction();
+
+			// UPDATEクエリ作成
+			$sql = "UPDATE product Set price = :price WHERE product_id = :id";
+
+			// prepareメソッドによるクエリの実行準備をする
+			$stmt = $db->prepare( $sql );
+			// 値をバインドする
+			$stmt->bindValue( ":price", 140 );
+			$stmt->bindValue( ":id", 1 );
+
+			// クエリ実行
+			$stmt->execute();
+			// 件数取得
+			$iRow = $stmt->rowCount();
+			// 更新件数表示
+			echo $iRow . "件更新しました。<br>";
+			// コミット
+			$db->commit();
+		}
+		catch( PDOException $e )
+		{
+			// エラー発生
+			echo $iRow . "エラーです。<br>";
+			// エラーメッセージ表示
+			echo $e->getMessage() . "<br>";
+			// ロールバック
+			$db->rollBack();
+		}
+
+		// SELECTクエリ
+		$sql = "SELECT product_id, product_name, price FROM product WHERE price >= ? AND product_id >= ?";
+		$stmt = $db->prepare( $sql );
+		// prepareメソッドによるクエリの実行準備をする
+		$stmt = $db->prepare( $sql );
+		// 値をバインドする
+		$stmt->bindValue( "1", 140 );
+		$stmt->bindValue( "2", 1 );
+
+		// クエリ実行
+		$stmt->execute();
+		// 件数取得
+		$iRow = $stmt->rowCount();
+		// 更新件数表示
+		echo $iRow . "件取得しました。<br>";
+		// 結果を取ってくる
+		if( $result = $stmt->fetchAll() )
+		{
+			// 結果が存在したら
+			// 連想配列を取得
+			foreach( $result as $arrRow )
+			{
+				echo $arrRow["product_id"] . "：" . $arrRow["product_name"] . "：" . $arrRow["price"] . "<br>";
+			}
+
+		}
+	?>
+</body>
+</html>
