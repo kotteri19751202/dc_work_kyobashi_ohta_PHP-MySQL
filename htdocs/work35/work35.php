d42bea48abb84b8bab26c72dbbfeb23a772adde7
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-08-08 11:49:51 +0900

    work35：関数
    関数を使用した下記のプログラムを作成してください。
    
    1~10の乱数を引数として渡し、引数が偶数の場合はその数に10を掛け、奇数の場合は100を掛けて、計算結果を返す関数を作成
    関数を呼び出し、echoなどで画面上に表示させる

---
+++ htdocs/work35/work35.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TRY49</title>
+</head>
+<body>
+	<?php
+	// 関数
+	function calcNum( $iNum )
+	{
+		$iResult;
+
+		// 偶数の時
+		if( $iNum %2 == 0 )
+		{
+			// 10倍
+			$iResult = $iNum * 10;
+		}
+		else // 奇数の時
+		{
+			// 100倍
+			$iResult = $iNum * 100;
+		}
+
+		return $iResult;
+	}
+
+	// ランダム値取得
+	$iRandNum = rand( 1, 10 );
+	// 関数呼び出し
+	echo calcNum( $iRandNum ) . "<br>";
+
+
+	?>
+</body>
+</html>
