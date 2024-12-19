f2c2cc427d5d69ce06a1e053ea78060d3a2b11e1
kotteri19751202 <53858636+kotteri19751202@users.noreply.github.com>
2024-12-19 15:10:18 +0900

    最終課題：パスワードのハッシュ化に対応
    ・パスワードのハッシュ化に対応
    
    ・DBに保存されてる以前のパスワードもハッシュ化した。

--- include/ec_site/model/user_model.php
+++ include/ec_site/model/user_model.php
@@ -31,35 +31,45 @@ function login( $db )
 	{
 		// Selectクエリ作成
 		$strQuery = "SELECT * FROM " . TABLE_NAME_USER 
-					. " WHERE UserName = :UserName AND Password = :Password";
+					. " WHERE UserName = :UserName";
 
 		// クエリの実行準備をする
 		$stmt = $db->prepare( $strQuery );
 		// 値をバインドする
 		$stmt->bindValue( ":UserName", $_POST["user_name"] );
-		$stmt->bindValue( ":Password", $_POST["password"] );
-
+	
 		// クエリ実行
 		$stmt->execute();
 		// 件数取得
 		$iRow = $stmt->rowCount();
-		// 件数が1件だったら
-		if( $iRow == 1 )
+		// 件数が1件じゃなかったら失敗
+		if( $iRow != 1 )
 		{
-			// 結果を取ってくる
-			if( $result = $stmt->fetchAll() )
-			{
-				dprint( "ユーザーID：" . $result[0]["UserID"] );
-				// セッションに情報保存
-				$_SESSION["user_id"] 	= $result[0]["UserID"];
-				$_SESSION["user_name"] 	= $result[0]["UserName"];
-				$_SESSION["is_admin"] 	= $result[0]["IsAdmin"];
-			}
-			return true;
+			dprint( "ログイン結果が1件じゃない" );
+			return false;
 		}
+
+		// 結果を取ってくる
+		$result = $stmt->fetchAll();
+		
+		// パスワードが一致しなかったら
+		if( !password_verify( $_POST["password"], $result[0]["Password"] ) )
+		{
+			dprint( "パスワードが一致しない" );
+			dprint( "POST:" . $_POST["password"] );
+			dprint( "HASH:" . $result[0]["Password"] );
+			
+			return false;
+		}
+
+		dprint( "ユーザーID：" . $result[0]["UserID"] );
+		// セッションに情報保存
+		$_SESSION["user_id"] 	= $result[0]["UserID"];
+		$_SESSION["user_name"] 	= $result[0]["UserName"];
+		$_SESSION["is_admin"] 	= $result[0]["IsAdmin"];
 				
-		// ログイン失敗
-		return false;
+		// ログイン成功
+		return true;
 	}
 	catch( PDOException $e )
 	{
@@ -93,6 +103,9 @@ function registUser( $db, &$strMsg )
 		return false;
 	}
 
+	// パスワードのハッシュ化
+	$strHashPassword = password_hash( $_POST["password"], PASSWORD_DEFAULT );
+
 	// レコードの挿入
 	try
 	{
@@ -107,7 +120,7 @@ function registUser( $db, &$strMsg )
 		$stmt = $db->prepare( $strQuery );
 		// 値をバインドする
 		$stmt->bindValue( ":UserName", $_POST["user_name"] );
-		$stmt->bindValue( ":Password", $_POST["password"] );
+		$stmt->bindValue( ":Password", $strHashPassword );
 
 		// クエリ実行
 		$stmt->execute();
